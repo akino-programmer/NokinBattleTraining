cb338696a70a0268cbc523a9bcab135ca33291f5
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-07-01 06:36:53 +0900

    get twitter's myProfile.

--- app/src/main/java/com/androidapp/akira/nokinbattletraining/EasyModeResultActivity.java
+++ app/src/main/java/com/androidapp/akira/nokinbattletraining/EasyModeResultActivity.java
@@ -10,6 +10,11 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
+
 public class EasyModeResultActivity extends AppCompatActivity {
 
     @Override
@@ -57,5 +62,62 @@ public class EasyModeResultActivity extends AppCompatActivity {
                 startActivity(intent);
         }
         });
+
+
+
+        Twitter twitter = new TwitterFactory().getInstance();
+           //Twitterオブジェクト作成
+
+        twitter.setOAuthConsumer("consumer key","consumer secret");
+           //Twitterオブジェクトにアプリケーションのconsumer keyとconsumer secretをセットする
+
+        AccessToken accessToken = new AccessToken(
+                        "Access token", // 自分のAccess token
+                        "Access token key"); // 自分のAccess token secret
+
+        twitter.setOAuthAccessToken(accessToken);
+           //自分のアクセストークンを作成し、Twitterオブジェクトにセットする。
+
+        User user = twitter.verifyCredentials();
+          //Userオブジェクトを作成
+
+        System.out.println(user.getId());//自分のアカウントのIDの取得（数字のID）
+
+        System.out.println(user.getName());//自分のアカウントの名前を取得
+
+        System.out.println(user.getScreenName());//自分のアカウントのUserNameを取得（アルファベットのみの名前）
+
+        System.out.println(user.getLocation());//自分のアカウントのプロフィールの場所を取得
+
+        System.out.println(user.getDescription());//自分のアカウントのプロフィールの説明を取得
+
+        System.out.println(user.getProfileImageURL());//自分のアカウントのプロフィール画像のURLを取得
+
+        System.out.println(user.getURL());//自分のアカウントのプロフィールのURLを取得する
+
+        System.out.println(user.isProtected());//自分のアカウントに鍵がついてるか取得する
+
+        System.out.println(user.getFollowersCount());//自分のアカウントのフォロワー数を取得する
+
+        System.out.println(user.getFriendsCount());//自分のアカウントのフォロー数を取得する
+
+        System.out.println(user.getCreatedAt());//自分のアカウントの登録日を取得する
+
+        System.out.println(user.getFavouritesCount());//自分のアカウントのお気に入り数を取得する
+
+        System.out.println(user.getProfileBannerURL());//自分のアカウントのバナー画像を取得する
+
+        System.out.println(user.getStatusesCount()); //呟きの数を取得
+
+        System.out.println(user.getListedCount());//追加されているリストの数を取得する
+
+        System.exit(0);
+
+
+
+
+
+
+
     }
 }
